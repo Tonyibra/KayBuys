57e0359ea190e9d01571827118438413db546904
Tony ibrahim <tonyibra.ads@gmail.com>
2022-05-21 18:20:06 +0300

    Initial commit

---
+++ variables/color.js
@@ -0,0 +1,48 @@
+export const COLORS = {
+  primary: "#78d35e",
+  primary_soft: "#43aee7",
+  bg_primary: "#ffd1d6",
+
+  button: {
+    disabled: "#e6e6e6",
+    active: "#78d35e",
+  },
+
+  bg_splash: "#78d35e",
+
+  badges: {
+    _top: "#f39f0a",
+    featured: "#e5f8f7",
+    _bump_up: "#f39f0a",
+    "is-sold": "#1aa78e",
+    "is-popular": "#14e0ad",
+  },
+
+  cardBg: {
+    _top: "#ffe3b0",
+    featured: "#e5f8f7",
+    "is-popular": "#c4fff0",
+  },
+
+  bg_light: "#f9f9f9",
+  bg_dark: "#f0f0f0",
+  text_gray: "#838383",
+  text_dark: "#111",
+  white: "#fff",
+  black: "#000",
+  gray: "gray",
+  blue: "#00f",
+  red: "#f00",
+  dark_green: "#155724",
+  green: "#25A01B",
+  light_green: "#d4edda",
+  yellow: "yellow",
+  yellow_dark: "#e0cb2d",
+  dodgerblue: "dodgerblue",
+  orange: "#ff6600",
+  border_light: "#cfcfcf",
+  border_dark: "gray",
+
+  packageCardHeader: "#8a6fdf",
+  rating_star: "#ffb400",
+};
